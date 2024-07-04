a3b54385a32486229a89c359318d8080a3fc3be1
Ramfi Aogusto <ramfiaogusto@gmail.com>
2024-07-04 17:46:09 -0400

    primer commit

---
+++ client/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h2 className='bg-slate-800'>hola</h2>
+  </React.StrictMode>,
+)
